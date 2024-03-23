f95ea86bcd73be340bae0a3c61988989f92c3f00
Ganth1811 <quangthangngo181@gmail.com>
2024-03-23 16:25:14 +0700

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -9,13 +9,13 @@ struct board{
         this->height = height;
         this->width = width;
 
-        this->letter_board = new char*[height];
+        this->letter_board = new char*[height + 2];
         for (int i = 0; i < height; i++)
-            this->letter_board[i] = new char[width];
+            this->letter_board[i] = new char[width + 2];
     }
 
     ~board(){
-        for (int i = 0; i < this->height; i++)
+        for (int i = 0; i < this->height + 2; i++)
             delete this->letter_board[i];
 
         delete this->letter_board;
@@ -23,12 +23,12 @@ struct board{
     }
 
     bool isInGameBoard(Coordinate p){
-        return (p.y < this->height && p.y >= 0 && p.x < this->width && p.x >= 0);
+        return (p.y < this->height + 1 && p.y >= 1 && p.x < this->width + 1 && p.x >= 1);
     }
 
     void printBoard(){
-        for (int i = 0; i < this->height; i++){
-            for (int j = 0; j < this->width; j++){
+        for (int i = 0; i < this->height + 2; i++){
+            for (int j = 0; j < this->width + 2; j++){
                 cout << this->letter_board[i][j] << " ";
             }
             cout << endl;
@@ -36,14 +36,14 @@ struct board{
     }
 
     bool isBoardEmpty(){
-        for (int i = 0; i < this->height; i++){
-            for (int j = 0; j < this->width; j++){
+        for (int i = 1; i < this->height + 1; i++){
+            for (int j = 1; j < this->width + 1; j++){
                 if (this->letter_board[i][j] != 0) return 0;
             }
         }
         return 1;
     }
 
-    
+
 
 };
