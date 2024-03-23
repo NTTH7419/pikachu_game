b432f962ad14c67ba8204859914fd67103e9e842
Ganth1811 <quangthangngo181@gmail.com>
2024-03-23 16:00:32 +0700

    updated board struct

--- board.cpp
+++ board.cpp
@@ -13,4 +13,37 @@ struct board{
         for (int i = 0; i < height; i++)
             this->letter_board[i] = new char[width];
     }
+
+    ~board(){
+        for (int i = 0; i < this->height; i++)
+            delete this->letter_board[i];
+
+        delete this->letter_board;
+
+    }
+
+    bool isInGameBoard(Coordinate p){
+        return (p.y < this->height && p.y >= 0 && p.x < this->width && p.x >= 0);
+    }
+
+    void printBoard(){
+        for (int i = 0; i < this->height; i++){
+            for (int j = 0; j < this->width; j++){
+                cout << this->letter_board[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    bool isBoardEmpty(){
+        for (int i = 0; i < this->height; i++){
+            for (int j = 0; j < this->width; j++){
+                if (this->letter_board[i][j] != 0) return 0;
+            }
+        }
+        return 1;
+    }
+
+    
+
 };
